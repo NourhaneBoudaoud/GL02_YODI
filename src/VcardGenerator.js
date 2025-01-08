936081c2a79520f8f9ce441a2a76bfbd6bb6a5a8
lbenoa <lebastardenoa@gmail.com>
2025-01-08 14:39:12 +0100

    Update VcardGenerator.js
    resolving issue

--- src/VcardGenerator.js
+++ src/VcardGenerator.js
@@ -4,12 +4,17 @@ const inquirer = require("inquirer");
 const readline = require("readline");
 const fs = require("fs-extra");
 
+/**
+ * Fonction pour générer un fichier contenant les informations VCard.**
+ * @param {string} vCard - Le contenu formaté de la VCard à écrire dans un fichier.
+ */
+
 async function generateVCardFile(vCard) {
     const dirPath = path.join(process.cwd(), './data');
     const VcardFilePath = path.join(dirPath, `Vcard-${new Date().toISOString().replace(/[:.]/g, "-")}.vcf`);
     console.log(VcardFilePath);
     try {
-        await fs.ensureDir(dirPath);
+        await fs.ensureDir(dirPath); // S'assure que le répertoire "data" existe, sinon il le crée.
         // Écriture du fichier Vcard
         fs.writeFileSync(VcardFilePath, vCard, "utf8");
         console.log(chalk.green(`Fichier Vcard généré avec succès : ${VcardFilePath}`));
@@ -17,6 +22,11 @@ async function generateVCardFile(vCard) {
         console.error(chalk.red("Erreur lors de la génération du fichier GIFT :"), error);
     }
 }
+/**
+ * Fonction pour créer une VCard à partir des informations utilisateur collectées via des invites.**
+ * @param {Object} contact - Objet contenant les informations initiales pour la VCard (par défaut vide).
+ * @returns {string} - La VCard générée au format texte.
+ */
 
 async function generateVCard(contact = {
     name : "",
@@ -61,5 +71,5 @@ contact.phone= phone;
 }
 
 module.exports = {
-    generateVCard
+    generateVCard // Exporte la fonction pour qu'elle soit utilisable dans d'autres fichiers.
 };
